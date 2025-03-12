8a7394978eaa566d1f4c63ba67c851cd2bda4601
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2025-03-12 10:18:41 +0300

    Initial commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    // public function  __construct() {
+    //     $this->middleware(['auth']);
+    // }
+
+    public function index() {
+
+
+        $username = Auth::user()->name;
+        $title = 'RGS';
+        return view('dashboards.index', [
+            'name' => $username,
+            'title' => $title
+        ]);
+    }
+}
